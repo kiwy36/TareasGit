161923df18d581003ec1b05146fb29f84ab16052
kiwy36 <kevinwittner92@gmail.com>
2025-05-27 12:45:34 -0300

    recopilacion calse 11 para la 12

---
+++ claseDoce/index.js
@@ -0,0 +1,17 @@
+const cuentas = [
+    {
+        id: 121,
+        nombre: 'cuenta A',
+        saldo: 33,
+    },
+    {
+        id: 223131,
+        nombre: 'cuenta B',
+        saldo: 50,
+    },
+    {
+        id: 3453535,
+        nombre: 'cuenta C',
+        saldo: 80,
+    },
+];
